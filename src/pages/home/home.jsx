ba75650ac3e52a7bc23bd3a186140c666b94a4cb
Tural Şükürov <tural.shkrov@gmail.com>
2023-03-13 17:58:17 +0400

    feat: homepage meal-type section

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,26 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from '../../components/navbar/navbar';
+import types from'../../data/mealTypes.json';
 import './home.css';
 
 export default function Home() {
-  const apiUrl = 'https://api.edamam.com/api/recipes/v2?type=public';
-  const apiParams = 'app_id=9a1cb042&app_key=0db6324d573590aaace93aca7be99d18';
-
-  const [ cakeRecipes, setCakeRecipes ] = useState();
-  const [ cocktailRecipes, setCocktailRecipes ] = useState();
-
-  useEffect(() => {
-    fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=cocktail&app_id=9a1cb042&app_key=0db6324d573590aaace93aca7be99d18`)
-    .then(res => res.json())
-    .then(data => setCakeRecipes(data.hits))
-  }, [])
 
   return (
     <div className='page'>
         <Navbar />
         <main>
             <h1 className="home-page-title">Browse new tastes</h1>
-        
+        <section id="meal-types">
+          {
+            types.map(type => {
+              return(
+                <div key={type.id} className="type-card">
+                  <img src={type.cover} alt={type.title} />
+                  <p>{type.title}</p>
+                </div>
+              )
+            })
+          }
+        </section>
         </main>
     </div>
   )
